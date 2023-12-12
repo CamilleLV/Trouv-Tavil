b8f7ebd6814b136fc42956254698a2158f217049
Léo <leo.coste@etu.univ-lyon1.fr>
2023-12-12 11:57:05 +0100

    mise en place du logo, de la liste déroulante, de la range

--- views/v_test.php
+++ views/v_test.php
@@ -11,8 +11,13 @@
 </head>
 
 <body>
+    <div id="sticky">
+        <a href="./">
+    <img src="assets\img\logo.png" width="75px" height="75px"/>
+</a>
+        <h1 id="titleTrouv">Trouv'</h1><h1 id="titleTavil">Tavil</h1>
+    </div>
     <header>
-        <h1>Trouv'Tavil</h1>
         <form method="POST">
             <div>
                 <h2>Recherche</h2>
@@ -77,18 +82,111 @@
                     <button>
                         Zone géographique
                     </button>
-                    <select placeholder="Search..">
-                        <option value="all">Toute la france</option>
-                    </select>
+                    <?php
+
+                // Remplacer cette partie avec le code pour charger les données JSON
+                $data = json_decode(file_get_contents('assets\json\departements-region.json'), true);
+
+                // Regrouper les départements par région
+                $regions = [];
+                foreach ($data as $entry) {
+                    $regions[$entry['region_name']][] = $entry;
+                }
+                
+                // Créer la liste déroulante HTML
+                echo '<form action="traitement.php" method="post">';
+                echo '<select name="departement">';
+                echo '<option value="France">Toute la france</option>';
+                foreach ($regions as $region => $departements) {
+                    echo '<optgroup label="' . htmlspecialchars($region) . '">';
+                    foreach ($departements as $departement) {
+                        echo '<option value="' . htmlspecialchars($departement['num_dep']) . '">' . htmlspecialchars($departement['dep_name']) . '</option>';
+                    }
+                    echo '</optgroup>';
+                }
+                echo '</select>';
+
+                ?>
                 </div>
 
                 <div class="slidecontainer">
-                    <p>nombre d'habitants</p>
-                <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+                    <p>nombre d'habitants : </p>
+                    <p><span id="value1"></span></p>
+                    <div class="slider" id="slider">
+                        <div class="slider-track"></div>
+                        <div class="slider-range" id="slider-range"></div>
+                        <div class="slider-thumb" id="thumb1"></div>
+                        <div class="slider-thumb" id="thumb2"></div>
+                    </div>
+                    <p><span id="value2"></span></p>
+                    <input type="hidden" id="inputValue1" name="habMin">
+                    <input type="hidden" id="inputValue2" name="habMax">
+                    <script>
+                        var slider = document.getElementById('slider');
+                        var range = document.getElementById('slider-range');
+                        var thumb1 = document.getElementById('thumb1');
+                        var thumb2 = document.getElementById('thumb2');
+                        var value1 = document.getElementById('value1');
+                        var value2 = document.getElementById('value2');
+
+                        var min = 0;
+                        var max = 2500000;
+
+                        thumb1.style.left = '0px';
+                        thumb2.style.left = slider.offsetWidth - thumb2.offsetWidth + 'px';
+                        updateRange();
+
+                        thumb1.addEventListener('mousedown', function(event) {
+                            window.addEventListener('mousemove', onMouseMove1);
+                            window.addEventListener('mouseup', onStopMove);
+                        });
+
+                        thumb2.addEventListener('mousedown', function(event) {
+                            window.addEventListener('mousemove', onMouseMove2);
+                            window.addEventListener('mouseup', onStopMove);
+                        });
+
+                        function onMouseMove1(event) {
+                            var newLeft = event.clientX - slider.getBoundingClientRect().left - thumb1.offsetWidth / 2;
+                            newLeft = Math.min(newLeft, parseInt(thumb2.style.left) - thumb1.offsetWidth);
+                            newLeft = Math.max(newLeft, 0);
+
+                            thumb1.style.left = newLeft + 'px';
+                            updateRange();
+                        }
+
+                        function onMouseMove2(event) {
+                            var newLeft = event.clientX - slider.getBoundingClientRect().left - thumb2.offsetWidth / 2;
+                            newLeft = Math.max(newLeft, parseInt(thumb1.style.left) + thumb2.offsetWidth);
+                            newLeft = Math.min(newLeft, slider.offsetWidth - thumb2.offsetWidth);
+
+                            thumb2.style.left = newLeft + 'px';
+                            updateRange();
+                        }
+
+                        function onStopMove() {
+                            window.removeEventListener('mousemove', onMouseMove1);
+                            window.removeEventListener('mousemove', onMouseMove2);
+                            window.removeEventListener('mouseup', onStopMove);
+                        }
+
+                        function updateRange() {
+                            var leftValue = Math.round((parseInt(thumb1.style.left) / slider.offsetWidth) * max);
+                            var rightValue = Math.round((parseInt(thumb2.style.left) / slider.offsetWidth) * max);
+
+                            value1.textContent = leftValue;
+                            value2.textContent = rightValue;
+
+                            range.style.left = thumb1.style.left;
+                            range.style.right = slider.offsetWidth - thumb2.offsetLeft - thumb2.offsetWidth + 'px';
+
+                            document.getElementById('inputValue1').value = leftValue;
+                            document.getElementById('inputValue2').value = rightValue;
+                        }
+                    </script>
                 </div>
                 <div div="centered">
                     <input type="submit" class="research" value="Recherche rapide" name="submit">
-
                     </input>
                 </div>
 
@@ -102,7 +200,7 @@
                 <ul>
                     <?php
                     if (isset($_POST['submit'])) {
-                        
+                        var_dump($_POST);
                     }
                     foreach ($result as $ville) {
 
